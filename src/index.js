33b8f0be98604ba40ab3b723a43f91d55b8eb6a9
Steve Godin <steve.god@gmail.com>
2018-06-06 14:10:24 -0700

    Add demo for Question Details

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<p>Hello, Bob!</p>, document.getElementById("root"));
+// All React component names must begin with a capital letter
+function QuestionDetails() {
+  return (
+    <div className="QuestionDetails">
+      <h1>What is your favourite colour?</h1>
+      <p>Red, green, blue, magenta, yellow, cyan, etc.</p>
+      <p>By Bridge Troll</p>
+      <p>
+        <strong>View Count:</strong> 111
+      </p>
+      <p>
+        <strong>Created at:</strong> 2018-01-01
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<QuestionDetails />, document.getElementById("root"));
 registerServiceWorker();
