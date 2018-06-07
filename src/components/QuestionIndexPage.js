e811ac610f41cea00556fda9017cc43641e52ca1
Steve Godin <steve.god@gmail.com>
2018-06-07 13:42:44 -0700

    Add solution State for Index

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -3,12 +3,22 @@ import Field from "./Field";
 import allQuestions from "../data/allQuestions";
 
 class QuestionIndexPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: [...allQuestions]
+      // using to create copy of allQuestions' to avoid mutating it
+      // when mutating the error.
+    };
+  }
+
   render() {
     return (
       <main className="QuestionIndexPage">
         <h1>Questions</h1>
         <ul style={{ padding: 0, listStyle: "none" }}>
-          {allQuestions.map(question => (
+          {this.state.questions.map(question => (
             <li key={question.id}>
               <a href="">{question.title}</a>
               <Field name="Author" value={question.author.full_name} />
