2d88de2c0d71f10f55c8ee30e99e0ceb10d4076a
Steve Godin <steve.god@gmail.com>
2018-06-07 13:16:30 -0700

    Add demo for Question Show to Class

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,15 +1,20 @@
-import React from "react";
+// In the following import, the first import is export as a default
+// by the "react" package. The second import within braces {} is called a named
+// import and was exported without the "default" keyword by the "react" package.
+import React, { Component } from "react";
 import QuestionDetails from "./QuestionDetails";
 import AnswerList from "./AnswerList";
 import detailedQuestion from "../data/detailedQuestion";
 
-function QuestionShowPage() {
-  return (
-    <main className="QuestionShowPage">
-      <QuestionDetails {...detailedQuestion} />
-      <AnswerList answers={detailedQuestion.answers} />
-    </main>
-  );
+class QuestionShowPage extends Component {
+  render() {
+    return (
+      <main className="QuestionShowPage">
+        <QuestionDetails {...detailedQuestion} />
+        <AnswerList answers={detailedQuestion.answers} />
+      </main>
+    );
+  }
 }
 
 export default QuestionShowPage;
