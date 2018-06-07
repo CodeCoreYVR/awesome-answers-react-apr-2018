35fee8972fdb50ed8e0a6f7796d8f1df1d2a2020
Steve Godin <steve.god@gmail.com>
2018-06-07 13:19:57 -0700

    Add solution for Question Index to Class

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import Field from "./Field";
 import allQuestions from "../data/allQuestions";
 
-function QuestionIndexPage(props) {
-  return (
-    <main className="QuestionIndexPage">
-      <h1>Questions</h1>
-      <ul style={{ padding: 0, listStyle: "none" }}>
-        {allQuestions.map(question => (
-          <li key={question.id}>
-            <a href="">{question.title}</a>
-            <Field name="Author" value={question.author.full_name} />
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+class QuestionIndexPage extends Component {
+  render() {
+    return (
+      <main className="QuestionIndexPage">
+        <h1>Questions</h1>
+        <ul style={{ padding: 0, listStyle: "none" }}>
+          {allQuestions.map(question => (
+            <li key={question.id}>
+              <a href="">{question.title}</a>
+              <Field name="Author" value={question.author.full_name} />
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
 }
 
 export default QuestionIndexPage;
