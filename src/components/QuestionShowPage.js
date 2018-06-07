a9a455956a534ed1b1f645fa4993ee01d91f2c41
Steve Godin <steve.god@gmail.com>
2018-06-07 13:32:37 -0700

    Add demo for Using State

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -7,11 +7,22 @@ import AnswerList from "./AnswerList";
 import detailedQuestion from "../data/detailedQuestion";
 
 class QuestionShowPage extends Component {
+  constructor(props) {
+    // When using a constructor in a class-based component,
+    // you must call super with props to allow React to properly
+    // initialize your component.
+    super(props);
+
+    this.state = {
+      question: { ...detailedQuestion }
+    };
+  }
+
   render() {
     return (
       <main className="QuestionShowPage">
-        <QuestionDetails {...detailedQuestion} />
-        <AnswerList answers={detailedQuestion.answers} />
+        <QuestionDetails {...this.state.question} />
+        <AnswerList answers={this.state.question.answers} />
       </main>
     );
   }
