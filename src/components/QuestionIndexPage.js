2fcb30cfc9f2fbf2cf87baec53c8cdc17fd90987
Steve Godin <steve.god@gmail.com>
2018-06-07 14:38:04 -0700

    Add demo for Delete for Questions Index

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -11,6 +11,28 @@ class QuestionIndexPage extends Component {
       // using to create copy of allQuestions' to avoid mutating it
       // when mutating the error.
     };
+
+    // To retain `this` inside a method when its function as a callback,
+    // we must bind it using the `bind` method of functions.
+    // `bind` will return a new function that is a copy of the original
+    // where its `this` is permanently set.
+    this.deleteQuestion = this.deleteQuestion.bind(this);
+  }
+
+  deleteQuestion(event) {
+    const { currentTarget } = event;
+    const { questions } = this.state;
+
+    // Any html attribute that begins with "data-" will be accessible
+    // through the .dataset property on the node where the property
+    // is the text that follows "data-id"
+    // data-target => .dataset.target
+    // data-id => .dataset.id
+    const questionId = parseInt(currentTarget.dataset.id, 10);
+
+    this.setState({
+      questions: questions.filter(q => q.id !== questionId)
+    });
   }
 
   render() {
@@ -21,7 +43,12 @@ class QuestionIndexPage extends Component {
           {this.state.questions.map(question => (
             <li key={question.id}>
               <a href="">{question.title}</a>
-              <Field name="Author" value={question.author.full_name} />
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <button data-id={question.id} onClick={this.deleteQuestion}>
+                  Delete
+                </button>
+                <Field name="Author" value={question.author.full_name} />
+              </div>
             </li>
           ))}
         </ul>
