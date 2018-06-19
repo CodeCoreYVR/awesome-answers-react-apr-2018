ce480f850f9b30949d542cd53f1184f7b1659a70
Steve Godin <steve.god@gmail.com>
2018-06-19 15:14:29 -0700

    Add demo for Not Found Page

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import QuestionIndexPage from "./QuestionIndexPage";
 import QuestionNewPage from "./QuestionNewPage";
 import SignInPage from "./SignInPage";
 import WelcomePage from "./WelcomePage";
+import NotFoundPage from "./NotFoundPage";
 import { Authenticator, Authenticate } from "./Authenticator";
 import AuthRoute from "./AuthRoute";
 import NavBar from "./NavBar";
@@ -32,6 +33,7 @@ class App extends Component {
               />
               <AuthRoute path="/questions/new" component={QuestionNewPage} />
               <Route path="/questions/:id" component={QuestionShowPage} />
+              <Route component={NotFoundPage} />
             </Switch>
           </div>
         </Router>
---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <main className="NotFoundPage">
+        <h1>🕵️‍♀️ 404 Not Found</h1>
+      </main>
+    );
+  }
+}
+
+export default NotFoundPage;
